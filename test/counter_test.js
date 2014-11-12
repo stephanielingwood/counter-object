6be695d1ff17146acc825789687470fc304c72f9
Stephanie Lingwood <stephanielingwood@gmail.com>
2014-11-11 17:43:45 -0800

    first commit

---
+++ test/counter_test.js
@@ -0,0 +1,21 @@
+/*jshint node: true */
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var Counter = require('../lib/counter.js');
+
+
+describe('Counter', function() {
+  var c = new Counter();
+
+  it('has a counter value of zero after the object is created', function(done) {
+    expect(c.returnCount()).to.eql(0);
+    done();
+  });
+
+  it('the counter value after an increment() will be one more than before', function(done) {
+    expect(c.increment()).to.eql(1);
+    done();
+  });
+});
